a783fe6af03b79cd92996c63e40ecc04d2d47d02
nipun <kancharlanipun@gmail.com>
2025-08-10 18:01:06 +0530

    added staticfiles in the settings

--- OJ_project/settings.py
+++ OJ_project/settings.py
@@ -129,9 +129,9 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 # Add this line to tell Django where to find your static files
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
